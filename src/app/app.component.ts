8d92c5c39a4faf3fcd89ae210e7eba267bd458b1
TiagoZampieri <tzampieri7@gmail.com>
2022-07-03 20:43:32 -0300

    Adição do eslint e correção em prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'm7-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
